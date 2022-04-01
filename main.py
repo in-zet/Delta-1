caa528266810db1a73911790382cc8445654b422
in-zet <didehddls234@gmail.com>
2022-04-01 18:04:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-d
+# 0401
+# selenium, webdriver-manager
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+driver.implicitly_wait(3)
+driver.get('https://google.com')
